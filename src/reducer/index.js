592ce5069c112268f66395bae91a5d68597c7274
SimpleaRU <info@simplea.ru>
2018-03-05 17:31:22 +0300

    start with redux

---
+++ src/reducer/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux'
+import reviewsReducers from '../reviews'
+
+const allReducers = combineReducers({
+    reviews: reviewsReducers
+})
+
+export default allReducers
