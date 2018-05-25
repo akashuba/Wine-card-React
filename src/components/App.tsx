d7f003f6c8a1d3cd550e193097e32f1b03b55561
Alexander Kashuba <info@simplea.ru>
2018-05-25 12:15:47 +0300

    Fetch JSON

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,6 +8,29 @@ function TitileElem() {
 }
 
 class App extends Component {
+ public componentDidMount() {
+    const status =  (response: any) => {
+      if (response.status !== 200) {
+        return Promise.reject(new Error(response.statusText))
+      }
+      return Promise.resolve(response)
+    }
+    const json = function(response: any) {
+      return response.json()
+    }
+    fetch('http://www.mocky.io/v2/5b07c8b43200008e00700034', {
+      method: 'post',
+      body: 'test=1'
+    })
+      .then(status)
+      .then(json)
+      .then( (data) => {
+        console.log('data', data)
+      })
+      .catch(function(error) {
+        console.log('error', error)
+      })
+  }
 
 public  render() {
     return (
