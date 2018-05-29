69df8d3d4e3b5da0b32453f7e1baf64396e3be0a
Alexander Kashuba <info@simplea.ru>
2018-05-29 11:02:30 +0300

    Delete clock component

--- src/components/Clock/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './clock.css'
-
-function Clock() {
-    const time = new Date().toLocaleTimeString()
-    return <div className="clock-to-center">{time}</div>
-}
-
-export default Clock
