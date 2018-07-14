12b0acd6617f47eefb5bf0b27606b3a1a741aba7
SimpleaRu <info@simplea.ru>
2018-07-14 12:36:32 +0300

    Add popup

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -3,6 +3,7 @@ import '../style.css'
 import { connect } from 'react-redux'
 import { filterBySparkling, filterByName, filterByColor, filterByTaste } from '../../AC'
 // import cards from '../../reduce
+import { WineAddPopup } from '../WineAddPopup/WineAddPopup'
 
 interface IProps {
   filterBySparklingWines: (isChecked: boolean) => void,
@@ -11,82 +12,98 @@ interface IProps {
   filterByTasteProps: (taste: string) => void,
 }
 
-class Filter extends Component<IProps> {
+interface IState {
+  showPopup: boolean
+}
+
+class Filter extends Component<IProps, IState> {
   private testInput: React.RefObject<HTMLInputElement>
   constructor(porps: IProps) {
     super(porps)
 
     this.testInput = React.createRef()
+    this.state = {
+      showPopup: false
+    }
+  }
+
+   public togglePopup = () => {
+    this.setState({ showPopup: !this.state.showPopup })
   }
 
   public render() {
     const { filterBySparklingWines, filterByNameProps, filterByColorProps, filterByTasteProps } = this.props
     return (
-      <section className="selectors">
-        <div className="color-selector">
-          <label htmlFor="color-selector" className="visibility-hidden">
-            Содержание сахара
+      <React.Fragment >
+        <section className="selectors">
+          <div className="color-selector">
+            <label htmlFor="color-selector" className="visibility-hidden">
+              Содержание сахара
                     </label>
-          <select name="color-selector" id="color-selector" className="selectors-arrow"
-            style={{ backgroundImage: `url(${require('../../img/icon-down-dir.svg')})` }}
-            onChange={(e) => filterByColorProps(e.target.value)}
-          >
-            <option value="">цвет</option>
-            <option value="red">красное</option>
-            <option value="white">белое</option>
-            <option value="rose">розовое</option>
-          </select>
-        </div>
-        <div className="taste-selector" >
-          <label htmlFor="taste-selector" className="visibility-hidden">
-            Содержание сахара
+            <select name="color-selector" id="color-selector" className="selectors-arrow"
+              style={{ backgroundImage: `url(${require('../../img/icon-down-dir.svg')})` }}
+              onChange={(e) => filterByColorProps(e.target.value)}
+            >
+              <option value="">цвет</option>
+              <option value="red">красное</option>
+              <option value="white">белое</option>
+              <option value="rose">розовое</option>
+            </select>
+          </div>
+          <div className="taste-selector" >
+            <label htmlFor="taste-selector" className="visibility-hidden">
+              Содержание сахара
                     </label>
-          <select name="taste-selector" id="taste-selector" className="selectors-arrow"
-            style={{ backgroundImage: `url(${require('../../img/icon-down-dir.svg')})` }}
-            onChange={(e) => filterByTasteProps(e.target.value)}
-          >
-            <option value="">сладость / крепость</option>
-            <option value="Сухое">сухое</option>
-            <option value="Полусухое">полусухое</option>
-            <option value="Полусладкое">полусладкое</option>
-            <option value="Десертное полусладкое">десертное полусладкое</option>
-            <option value="Десертное сладкое">десертное сладкое</option>
-            <option value="Сладкое">сладкое</option>
-            <option value="Ликерное">ликерное</option>
-            <option value="Крепкое">крепкое</option>
-          </select>
-        </div>
-        <div className="sparkling-selector">
-          <input
-            type="checkbox"
-            name="sparkling-selector"
-            id="sparkling-selector"
-            value="Игристое"
-            className="visibility-hidden"
-            onChange={(e) => filterBySparklingWines(e.target.checked)}
-          />
-          <label htmlFor="sparkling-selector">
-            игристое
+            <select name="taste-selector" id="taste-selector" className="selectors-arrow"
+              style={{ backgroundImage: `url(${require('../../img/icon-down-dir.svg')})` }}
+              onChange={(e) => filterByTasteProps(e.target.value)}
+            >
+              <option value="">сладость / крепость</option>
+              <option value="Сухое">сухое</option>
+              <option value="Полусухое">полусухое</option>
+              <option value="Полусладкое">полусладкое</option>
+              <option value="Десертное полусладкое">десертное полусладкое</option>
+              <option value="Десертное сладкое">десертное сладкое</option>
+              <option value="Сладкое">сладкое</option>
+              <option value="Ликерное">ликерное</option>
+              <option value="Крепкое">крепкое</option>
+            </select>
+          </div>
+          <div className="sparkling-selector">
+            <input
+              type="checkbox"
+              name="sparkling-selector"
+              id="sparkling-selector"
+              value="Игристое"
+              className="visibility-hidden"
+              onChange={(e) => filterBySparklingWines(e.target.checked)}
+            />
+            <label htmlFor="sparkling-selector">
+              игристое
                         <div className="checkbox-view" />
-          </label>
-        </div>
-        <div className="name-selector" >
-          <label htmlFor="search-string" className="visibility-hidden">
-            Поиск по названию
+            </label>
+          </div>
+          <div className="name-selector" >
+            <label htmlFor="search-string" className="visibility-hidden">
+              Поиск по названию
                     </label>
-          <input
-            type="text"
-            name="search-string"
-            id="search-string"
-            placeholder="Введите название вина"
-            ref={this.testInput}
-            onChange={(e) => filterByNameProps(e.target.value)}
-          />
-        </div>
-        {/* <button type="button" className="add-button" aria-label="добавить вино">
-          ДОБАВИТЬ
-        </button> */}
-      </section>
+            <input
+              type="text"
+              name="search-string"
+              id="search-string"
+              placeholder="Введите название вина"
+              ref={this.testInput}
+              onChange={(e) => filterByNameProps(e.target.value)}
+            />
+          </div>
+          <button type="button" className="add-button" aria-label="добавить вино"
+            onClick={this.togglePopup}
+          >
+            ДОБАВИТЬ
+        </button>
+        </section>
+        {this.state.showPopup ? <WineAddPopup togglePopup={this.togglePopup} /> : null}
+      </React.Fragment >
     )
   }
 }
---
+++ src/components/WineAddPopup/WineAddPopup.tsx
@@ -0,0 +1,112 @@
+import React from 'react'
+
+interface IProps {
+  togglePopup: () => void
+}
+
+const WineAddPopup = ({togglePopup}: IProps): React.ReactElement<IProps> => {
+
+  return (
+    <section className="add-wine-form-wrapper">
+      <div className="add-wine-form">
+        <h3 className="form-titile">Добавить карту</h3>
+        <form method="post" encType="multipart/form-data" name="addcard">
+          <ul>
+            <li>
+              <label htmlFor="contributor">Представьтесь</label>
+              <input type="text" name="contributor" id="contributor" required />
+            </li>
+            <li>
+              <label htmlFor="formName">Марка</label>
+              <input type="text" name="formName" id="formName" placeholder="Cabernet" required />
+            </li>
+            <li className="wineColor">
+              <label htmlFor="wineColor">Цвет </label>
+              <select name="wineColor" id="wineColor" className="selectors-arrow">
+                <option value="red">красное</option>
+                <option value="white">белое</option>
+                <option value="rose">розовое</option>
+              </select>
+            </li>
+            <li>
+              <label htmlFor="sugarContent">Содержание сахара</label>
+              <select name="sugarContent" id="sugarContent" className="selectors-arrow">
+                <option value="Сухое">сухое</option>
+                <option value="Полусухое">полусухое</option>
+                <option value="Полусладкое">полусладкое</option>
+                <option value="Десертное полусладкое">десертное полусладкое</option>
+                <option value="Десертное сладкое">десертное сладкое</option>
+                <option value="Сладкое">сладкое</option>
+                <option value="Ликерное">ликерное</option>
+                <option value="Крепкое">крепкое</option>
+              </select>
+            </li>
+            <li>
+              <input type="checkbox" name="sparkling" id="sparkling"
+                value="Игристое" className="form-sparkling-input visibility-hidden" />
+              <label htmlFor="sparkling" className="form-sparkling-lable">Игристое:
+            <div className="form-checkbox-view"></div>
+              </label>
+            </li>
+            <li>
+              <label htmlFor="formColorText">Цвет</label>
+              <input type="text" name="colorText" id="formColorText" required />
+            </li>
+            <li>
+              <label htmlFor="aromeText">Аромат</label>
+              <input type="text" name="aromeText" id="aromeText" required />
+            </li>
+            <li>
+              <label htmlFor="tasteText">Вкус</label>
+              <input type="text" name="tasteText" id="tasteText" required />
+            </li>
+            <li>
+              <label htmlFor="originText">Происхождение</label>
+              <input type="text" name="originText" id="originText" placeholder="Италия" required />
+            </li>
+            <li>
+              <label htmlFor="priceText">Цена &#8381;</label>
+              <input type="number" name="priceText" id="priceText" required />
+            </li>
+            <li id="raiting-wrapper">
+              <label htmlFor="formRaiting"> Рейтинг</label>
+              <select name="formRaiting" id="formRaiting" className="selectors-arrow">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+                <option>8</option>
+                <option>9</option>
+                <option>10</option>
+              </select>
+            </li>
+            <li>
+              <div className="noteText-wraper">
+                <label htmlFor="noteText">Комментарии:</label>
+                <textarea name="noteText" id="noteText">
+                </textarea>
+              </div>
+            </li>
+            <li id="file-button">
+              <label htmlFor="fileFild" className="fileFild">ВЫБРАТЬ ФОТО</label>
+              <input type="file" name="file" id="fileFild" className="visibility-hidden" />
+              <p className="file-text">(jpeg, png, bmp размер до 500 KB)</p>
+            </li>
+            <li>
+              <label htmlFor="formSubmit"></label>
+              <input type="submit" value="добавить" id="formSubmit" />
+            </li>
+          </ul>
+        </form>
+        <button type="button" aria-label="Закрыть форму" id="formClose"
+          onClick={togglePopup}
+        ></button>
+      </div>
+    </section>
+  )
+}
+
+export { WineAddPopup }
